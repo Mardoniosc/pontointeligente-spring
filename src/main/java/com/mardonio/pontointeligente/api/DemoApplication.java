58a8b48edc4ab996235f06f112a012c15d396212
Mardonio Silva da Costa <mrdonio.sc@gmail.com>
2020-02-21 09:41:53 -0300

    Configurado as entidades e criação das tabelas

--- src/main/java/com/mardonio/pontointeligente/api/DemoApplication.java
+++ src/main/java/com/mardonio/pontointeligente/api/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.mardonio.pontointeligente;
+package com.mardonio.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/mardonio/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.mardonio.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+  ROLE_ADMIN,
+  ROLE_USUARIO;
+}
